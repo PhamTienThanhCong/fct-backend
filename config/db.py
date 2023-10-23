97af1b81954b50cdb2b68759ea6111d4431ecb51
congpham <congpham@maxflowtech.com>
2023-10-23 11:30:36 +0700

    add requirements

--- config/db.py
+++ config/db.py
@@ -4,12 +4,12 @@ import os
 
 load_dotenv()
 
-
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DATABASE = os.getenv("DATABASE")
+
 connect_string = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(
     DB_USER, DB_PASS, DB_HOST, DB_PORT, DATABASE)
 
