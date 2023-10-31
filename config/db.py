28aba46a92bd374cf8b47028518142b092675cf0
congpham <congpham@maxflowtech.com>
2023-10-31 15:58:54 +0700

    new config

--- config/db.py
+++ config/db.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, MetaData
-from config.env_value import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+from config.env_value import DB_USER, DB_HOST, DB_PORT, DB_NAME
+
+load_dotenv()
+
+DB_PASS = os.getenv('DB_PASS')
 
 engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(DB_USER, "", DB_HOST, DB_PORT, DB_NAME))
 
--- config/env_value.py
+++ config/env_value.py
@@ -9,7 +9,6 @@ DESCRIPTION_APP= os.getenv('DESCRIPTION_APP')
 
 DB_USER = os.getenv('DB_USER')
 DB_NAME = os.getenv('DB_NAME')
-DB_PASS = os.getenv('DB_PASS')
 DB_PORT = os.getenv('DB_PORT')
 DB_HOST = os.getenv('DB_HOST')
 
