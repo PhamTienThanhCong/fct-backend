fa51476ea5681339a3050447d5704ea390a65b65
congphamtienthanh <congphamtienthanh@gmail.com>
2023-11-03 14:06:35 +0700

    fix upload

--- config/db.py
+++ config/db.py
@@ -5,7 +5,7 @@ from constants.env_value import DB_USER, DB_HOST, DB_PORT, DB_NAME
 
 load_dotenv()
 
-DB_PASS = os.getenv('DB_PASS')
+DB_PASS = os.getenv('DB_PASS') or ""
 
 engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME))
 
